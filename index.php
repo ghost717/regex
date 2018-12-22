9624573cde3670865e40a0d17b2178b654d4eab8
ghost717 <dawid.zawadzki7717@gmail.com>
2018-12-22 21:10:50 +0100

    testing

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
     echo '<br><br> #1 Character Classes <br> ';
+    $text = '102x768.jpg';
+    $text2 = 'p_bg_(1)-1024x768.jpg';
 
-
-    if(preg_match($pattern, $text)){
+    if(preg_match('/.+-[0-9]+x[0-9]+.jpg/', $text2)){
         echo "Match found!";
     } else{
         echo "Match not found.";
