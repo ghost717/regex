3dc62411eeed528ea27ba8b720f5e1ff731dc6f9
ghost717 <dawid.zawadzki7717@gmail.com>
2018-12-23 11:38:53 +0100

    new regex, testing

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
     echo '<br><br> #1 Character Classes <br> ';
     $text = '102x768.jpg';
     $text2 = 'p_bg_(1)-1024x768.jpg';
+    $text3 = 'XXXXR30P';
 
     if(preg_match('/.+-[0-9]+x[0-9]+.jpg/', $text2)){
+        echo "Match found!";
+    } else{
+        echo "Match not found.";
+    }
+
+    echo '<br>'.$text3;
+
+    if(preg_match('/.+XR[0-9]+P/', $text3)){
         echo "Match found!";
     } else{
         echo "Match not found.";
